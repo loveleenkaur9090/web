fde72e8401988c4e77cbf9eb70c7846cf65a205a
loveleenkaur99 <136727093+loveleenkaur99@users.noreply.github.com>
2024-08-31 15:28:40 +0530

    modified

--- Practice/Progress bar/javascript.js
+++ Practice/Progress bar/javascript.js
@@ -1,22 +1,29 @@
 // script.js
-let progress = 0;
-let progress2 = 0;
+let progress = 30;
+// let progress2 = 45;
+// let progress3 = 5;
 
 function increaseProgress() {
-    if (progress < 10) {
+    if (progress < 30) {
         progress += 30;
         document.getElementById('progress-bar').style.width = progress + '%';
+    }else{
+        alert("you are not eligible");
     }
 }
-function increaseProgress2() {
-    if (progress < 60 ) {
-        progress += 40;
-        document.getElementById('progress-bar').style.width = progress + '%';
-    }
-}
-function increaseProgress3() {
-    if (progress < 100 ) {
-        progress += 50;
-        document.getElementById('progress-bar').style.width = progress + '%';
-    }
-}
+
+
+
+    
+// function increaseProgress2() {
+//     if (progress < 45 ) {
+//         progress += 40;
+//         document.getElementById('progress-bar').style.width = progress + '%';
+//     }
+// }
+// function increaseProgress3() {
+//     if (progress < 65 ) {
+//         progress += 50;
+//         document.getElementById('progress-bar').style.width = progress + '%';
+//     }
+// }
