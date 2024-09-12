2a8d002ed70d19b886e526f21dd5441ea30c2aa2
Loveleen <kamaljeetpaul9107@gmail.com>
2024-09-12 12:22:21 +0530

    okay

---
+++ Project By SCS/Design Animation website/javascript.js
@@ -0,0 +1,40 @@
+gsap.to("#page",{
+    scroolTrigger:{
+        trigger:`#page-1`,
+        start:`top top`,
+        end:`bottom-top`,
+        markers:true,
+        pin:true,
+        scroller:`main`
+    }
+})
+gsap.to("#page-1",{
+    scroolTrigger:{
+        trigger:`#page-1`,
+        start:`top top`,
+        end:`bottom-top`,
+        markers:true,
+        pin:true,
+        scroller:`main`
+    }
+})
+gsap.to("#page-2",{
+    scroolTrigger:{
+        trigger:`#page-1`,
+        start:`top top`,
+        end:`bottom-top`,
+        markers:true,
+        pin:true,
+        scroller:`main`
+    }
+})
+gsap.to("#page-3",{
+    scroolTrigger:{
+        trigger:`#page-1`,
+        start:`top top`,
+        end:`bottom-top`,
+        markers:true,
+        pin:true,
+        scroller:`main`
+    }
+})
